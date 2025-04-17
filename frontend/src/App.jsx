29f7741d34177bc4db53f2f2075e980a87128bae
ucubz <ucubz@yahoo.com>
2025-04-17 13:43:04 +0700

    Update App.jsx
    merubah nama file dashboard.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Login from './pages/Login'
-import Dashboard from './pages/Dashboard'
+import Dashboard from './pages/dashboard'
 import PrivateRoute from './components/PrivateRoute'
 
 function App() {
