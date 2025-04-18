f7f2a5c0cf4062c3f4191e721e6a92577a087440
ucubz <ucubz@yahoo.com>
2025-04-18 16:50:30 +0700

    masih memperbaiki app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from "./pages/Login";
 import Dashboard from './pages/dashboard';
-import RequireAuth from '.RequireAuth';
+import RequireAuth from './RequireAuth'; // <- fix typo
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/Login" element={<Login />} />
+        <Route path="/login" element={<Login />} /> {/* <- lowercase route */}
         <Route
           path="/dashboard"
           element={
