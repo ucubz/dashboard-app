96c73b029973fd06140298bed5522d58b2280284
ucubz <ucubz@yahoo.com>
2025-04-19 15:08:16 +0700

    Create RequireAuth.jsx
    menambahkan file requireauth.jsx

---
+++ frontend/src/components/RequireAuth.jsx
@@ -0,0 +1,19 @@
+// src/components/RequireAuth.jsx
+import { Navigate } from 'react-router-dom';
+
+const RequireAuth = ({ children, allowedRoles }) => {
+  const token = localStorage.getItem('token');
+  const user = JSON.parse(localStorage.getItem('user'));
+
+  if (!token || !user) {
+    return <Navigate to="/" replace />;
+  }
+
+  if (!allowedRoles.includes(user.role)) {
+    return <Navigate to="/" replace />;
+  }
+
+  return children;
+};
+
+export default RequireAuth;
