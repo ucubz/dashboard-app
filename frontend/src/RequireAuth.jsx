377b6ba191d35f218f28be801b2fc663d1282f3c
ucubz <ucubz@yahoo.com>
2025-04-20 06:39:35 +0700

    Update RequireAuth.jsx

--- frontend/src/RequireAuth.jsx
+++ frontend/src/RequireAuth.jsx
@@ -1,16 +1,13 @@
-// src/RequireAuth.jsx
 import { Navigate } from 'react-router-dom';
 
 const RequireAuth = ({ children, allowedRoles }) => {
   const token = localStorage.getItem('token');
   const user = JSON.parse(localStorage.getItem('user'));
 
-  // Jika tidak login, redirect ke halaman login
   if (!token || !user) {
     return <Navigate to="/" replace />;
   }
 
-  // Jika role tidak sesuai, redirect ke halaman default
   if (!allowedRoles.includes(user.role)) {
     return <Navigate to="/" replace />;
   }
