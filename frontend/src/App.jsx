0e578c7fdbaf7cfdc6dcb0dd3f420acec4f8f818
ucubz <ucubz@yahoo.com>
2025-04-19 14:58:14 +0700

    Update App.jsx
    untuk menambahkan auth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,42 +1,37 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from "./pages/Login";
-import Dashboard from './pages/dashboard';
-import InputPengaduan from './pages/InputPengaduan';
-import DaftarPengaduan from './pages/DaftarPengaduan';
-import RequireAuth from './RequireAuth'; // ✅ ini yang kurang
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; import Login from "./pages/Login"; import Dashboard from './pages/dashboard'; import InputPengaduan from './pages/InputPengaduan'; import DaftarPengaduan from './pages/DaftarPengaduan'; import RequireAuth from './components/RequireAuth';
 
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route
-          path="/dashboard"
-          element={
-            <RequireAuth>
-              <Dashboard />
-            </RequireAuth>
-          }
-        />
-        <Route
-          path="/input-pengaduan"
-          element={
-            <RequireAuth>
-              <InputPengaduan />
-            </RequireAuth>
-          }
-        />
-        <Route
-          path="/daftar-pengaduan"
-          element={
-            <RequireAuth>
-              <DaftarPengaduan />
-            </RequireAuth>
-          }
-        />
-      </Routes>
-    </Router>
-  );
-}
+function App() { return ( <Router> <Routes> <Route path="/" element={<Login />} />
+
+<Route
+      path="/dashboard"
+      element={
+        <RequireAuth allowedRoles={['kepala_subdir', 'kepala_seksi']}>
+          <Dashboard />
+        </RequireAuth>
+      }
+    />
+
+    <Route
+      path="/input-pengaduan"
+      element={
+        <RequireAuth allowedRoles={['petugas_dashboard']}>
+          <InputPengaduan />
+        </RequireAuth>
+      }
+    />
+
+    <Route
+      path="/daftar-pengaduan"
+      element={
+        <RequireAuth allowedRoles={['kepala_subdir', 'kepala_seksi']}>
+          <DaftarPengaduan />
+        </RequireAuth>
+      }
+    />
+  </Routes>
+</Router>
+
+); }
 
 export default App;
+
