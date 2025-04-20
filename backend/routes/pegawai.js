1bd0f126047a7c5c5a333fada7ff8b0acb95b567
ucubz <ucubz@yahoo.com>
2025-04-20 20:33:48 +0700

    Create pegawai.js
    menambahkan routes pegawai.js

---
+++ backend/routes/pegawai.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const initPegawaiDB = require('../models/pegawaiDB');
+
+// GET - Ambil semua data pegawai
+router.get('/', async (req, res) => {
+  const db = await initPegawaiDB();
+  db.all('SELECT * FROM pegawai', [], (err, rows) => {
+    if (err) {
+      console.error('Gagal ambil data pegawai:', err.message);
+      return res.status(500).json({ error: 'Gagal ambil data pegawai' });
+    }
+    res.json(rows);
+  });
+});
+
+module.exports = router;
